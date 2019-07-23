e2f3a232b326710d0e2592e3a1a7fa2802d46249
Jacob Cook <jc2017@ic.ac.uk>
2019-07-23 17:06:56 +0100

    Halfway through first simulation attempt

--- Syntrophy/src/Syntrophy.jl
+++ Syntrophy/src/Syntrophy.jl
@@ -2,12 +2,40 @@ module Syntrophy
 
 # Can import or use other modules
 # Can also include code from other files other files
+using DifferentialEquations
 
 # Export objects and function that I want to be externally accessible
-export GFree
+export GFree, Nut, React, Microbe, ↦
 
 # Decleration of internally used constants
 Rgas = 8.31446261815324 # gas constant in J.K^-1.mol^-1
+
+# Structure to store nutrient identies, supply rates and washout fractions
+struct Nut
+    idt::Int64 # Identifying integer should be unique
+    α::Float64 # Supply rate mol ml^−1 s^−1
+    δ::Float64 # dilution rate s^-1
+end
+
+# Structure to store details of each reaction
+struct React
+    idt::Int64 # Identifying integer should be unique
+    sub::Array{Int64,1} # substrate identifier
+    subS::Array{Int64,1} # substrate stochiometry
+    prd::Array{Int64,1} # end product identifier
+    prdS::Array{Int64,1} # product stochiometry
+    ΔG0::Float64 # Gibbs free energy (in Joules) at standard condidtions
+end
+
+# Structure to store microbial strategies and populations
+struct Microbe
+    η::Float64 # free energy use strategy, mol of ATP per mol of substrate
+    m::Float64 # maintainance cost, mol of ATP per cell per second
+    reac::Int64 # Identity of reaction used to fuel
+end
+
+# Overloadable version of getfields
+↦(s, f) = getfield(s, f)
 
 # Function to calculate Gibbs free energy change (in Joules) of the reaction
 function GFree(subC::Array{Float64,1},subN::Array{Int64,1},prodC::Array{Float64,1},prodN::Array{Int64,1},Temp::Float64,ΔG0::Float64)
--- Syntrophy/src/test.jl
+++ Syntrophy/src/test.jl
@@ -1,17 +1,75 @@
 using Syntrophy
+using DifferentialEquations
 # This is a script to write my testing code into
 # Anything reusable should be moved into Syntrophy.jl as a seperate callable function
 
+function npops(du,u,p::Array{Float64,1},nuts::Array{Nut,1},reacs::Array{React,1},mics::Array{Microbe,1},t::Float64)
+    # Extract required parameters
+    Y = p[1]
+    Γ = p[2]
+    # Extract relevant data from vector of nutrients
+    α = nuts.↦:α
+    δ = nuts.↦:δ
+    N = length(nuts) # Number of nutrients
+    # And relevant data from vector of microbes
+    η = mics.↦:η
+    M = length(mics) # Number of microbes
+    # MUST CALCULATE q
+
+    # Do nutrients first
+    for i = 1:N
+        du[i] = α[i]-(δ[i]+sum(q[i,1:end]))*u[i]
+    end
+    # Then calculate population changes
+    for i = N+1:N+M
+        if E[i-N] >= 0.0 # find if growing or decaying
+            # NEED TO SORT OUT DILUTION RATE HERE
+            du[i] = E[i-N]*Y*u[i] - δ*u[i]
+        else
+            du[i] = -E[i-N]*Γ*u[i] - δ*u[i]
+        end
+    end
+    return(du)
+end
+
 function main()
-    # Need to find a suitable testing example tomorrow
-    T = 298.0 # Standard temp
-    sub = [ 2.00*10^(-4)]
-    prod = [ 3.00*10^(-6) ]
-    subS = [1]
-    prodS = [1]
-    ΔG0 = 7549.6429
-    ΔG = GFree(sub,subS,prod,prodS,T,ΔG0)
-    println(ΔG/4184)
+    # Nutrient variables
+    α = 4.70*10^(-9)
+    δ = 3.50*10^(-5)
+    # make nutrients
+    # 1 = glucose, 2 = oxegen, 3 = bicarbonate, 4 = hydrogen ion
+    nuts = [Nut(1,α,δ),Nut(2,6*α,δ),Nut(3,0,δ),Nut(4,0,0)]
+    # Now make reactions
+    ΔG0 = -2843800.0
+    reac = [React(1,[1,2],[1,6],[3,4],[6,6],ΔG0)]
+    # microbe variables
+    η1 = 1.0
+    η2 = 0.5
+    # maintainance equal
+    m = 2.16*10^(-19)
+    # both have same substrate and end product
+    r = 1
+    # make microbes
+    mics = [Microbe(η1,m,r),Microbe(η2,m,r)]
+    # Set intial populations and nutrient concentrations
+    pops = 0.001*ones(length(mics))
+    concs = zeros(length(nuts))
+    concs[1] = 1.00*10^(-9)
+    concs[2] = 2.70*10^(-7)
+    concs[3] = 1.00*10^(-9)
+    concs[4] = 1.00*10^(-7) # pH 7
+    # Define some constants
+    Y = 2.36*10^(13) # biomass yield cells per mol ATP
+    Γ = 1.16*10^(12) # starvation rate cells per mol ATP (deficit)
+    K = 2.00*10^(-10) # Saturation constant mol ml^(−1)
+    qm = 0.00 # NEED TO FIND THIS ONE
+    ΔGATP = 75000 # Gibbs free energy of formation of ATP in a standard cell
+    p = [Y,Γ,K,qm,ΔGATP]
+    u0 = [concs;pops]
+    tspan = (0.0,100.0)
+    # NEED TO WORK OUT HOW TO PASS THIS PROPERLY
+    prob = ODEProblem(npops,u0,p,nuts,reac,mics,tspan)
+    sol = solve(prob)
     return(nothing)
 end
 

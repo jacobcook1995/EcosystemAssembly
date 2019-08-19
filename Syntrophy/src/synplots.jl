42a9b5a659d37b46d5b5ab1faff6ae78916b7136
Jacob Cook <jc2017@ic.ac.uk>
2019-08-19 14:08:37 +0100

    synplots.jl actually works now

--- Syntrophy/src/synplots.jl
+++ Syntrophy/src/synplots.jl
@@ -6,6 +6,56 @@ import PyPlot
 
 # This is a script to store the functions that plot the figures shown in my Syntrophy SI
 
+# Function to update population and nutrient concentrations
+# This is run for a single population utilising a single reaction
+function singlepop(du::Array{Float64,1},u::Array{Float64,1},p::Array{Float64,1},nuts::Array{Nut,1},reacs::Array{React,1},
+                mic::Microbe,t::Float64)
+    # Extract required parameters
+    Y = p[1]
+    # Extract relevant data from vector of nutrients
+    α = nuts.↦:α
+    δ = nuts.↦:δ
+    con = nuts.↦:cst
+    N = length(nuts) # Number of nutrients
+    # And relevant data from vector of microbes
+    η = mic.η
+    m = mic.m # running for single microbe
+    M = 1 # Number of microbes
+    # Extract reaction stochiometry
+    stc = (reacs.↦:stc)[1]
+    ΔG0 = (reacs.↦:ΔG0)[1]
+    # Now calculate q
+    # p[2] = KS, p[3] = qm, p[4] = ΔGATP, p[5] = Temp
+    q = qrate(u[1:N],p[2],p[3],p[4],ΔG0,p[5],stc,η)
+    # Make vector to store nutrient changes due to consumption
+    δX = zeros(N)
+    for i = 1:length(stc)
+        for j = N+1:N+M
+            δX[i] += stc[i]*q*u[j] # Assumes single reaction
+        end
+    end
+    # q has no dependance on population
+    # Now update nutrients
+    for i = 1:N
+        if con[i] == false
+            du[i] = α[i]-δ[i]*u[i]+δX[i]
+        else
+            du[i] = 0
+        end
+    end
+    # Then calculate population changes
+    for i = N+1:N+M
+        j = i-N
+        E = netE(η,q,m)
+        if E >= 0.0 # find if growing or decaying
+            du[i] = E*Y*u[i] # No dilution rate so can ignore
+        else
+            du[i] = E*Y*u[i]
+        end
+    end
+    return(du)
+end
+
 # A function to make a plot of the maxcosump rate for a microbial population in glucose
 function maxcosump()
     # Nutrient variables

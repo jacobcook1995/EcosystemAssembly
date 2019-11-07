c7a2706503399dff47ef8abad861d5892aa1801c
Jacob Cook <jc2017@ic.ac.uk>
2019-11-07 09:50:04 +0000

    Moved parameterisation across

--- Syntrophy/src/test.jl
+++ Syntrophy/src/test.jl
@@ -88,14 +88,47 @@ function stead(KS::Float64,kr::Float64,η::Float64,qm::Float64,m::Float64,CO::Fl
     return(S,P,X)
 end
 
+# Function to demonstrate all 7 tradeoffs
+function tradeoffs()
+    # This is the parameterisation I used initially want to check that these values of q_m and KS are the same
+    # Nutrient variables
+    α = 5.55e-6
+    δ = 2.00e-4 # 1.00*10^(-4)
+    # make nutrients
+    # 1 = glucose, 2 = oxegen, 3 = bicarbonate, 4 = hydrogen ion
+    nuts = [Nut(1,false,α,δ),Nut(2,true,0,0),Nut(3,false,0,δ),Nut(4,true,0,0)]
+    # Now make reactions
+    ΔG0 = -2843800.0
+    reac = [React(1,[1,2,3,4],[-1,-6,6,6],ΔG0)]
+    # microbe variables
+    η = 38.0
+    r = 1 # Only reaction
+    m = 2.16*10^(-19) # maintainance
+    # Considering 1 microbe with no maintaince and no dilution
+    mics = Microbe(η,m,1,0.0)
+    # Set intial populations and nutrient concentrations
+    pops = 100.0
+    concs = zeros(length(nuts))
+    # define initial concentrations
+    concs[1] = 0.0555 # high initial concentration to ensure growth
+    concs[2] = 0.21 # High value so oxegen isn't limiting
+    concs[3] = 0.0 # No initial concentration
+    concs[4] = 1.00*10.0^(-7) # pH 7
+    # Define some constants
+    ΔGATP = 75000.0 # Gibbs free energy of formation of ATP in a standard cell
+    Temp = 312.0 # Temperature that growth is occuring at in Kelvin
+    # All other constants require quite a bit of defining
+    Y = 2.36*10.0^(13) # yield in cells per mole of ATP
+    KS = 2.40*10.0^(-5) # saturation constant (substrate)
+    qm = 3.42*10.0^(-18) # maximal rate substrate consumption mol cell s^-1
+    p = [Y,KS,qm,ΔGATP,Temp]
+    u0 = [concs;pops]
+    tspan = (0.0,5000000.0)
+    
+    return(nothing)
+end
 
-# ROUGH SOLUTION LOOK AT TOMMOROW
-# basically 3 don't involve changing thermodynamics (dissipation)
-# 4 do
-# Of these 2 are beneifical, if they can be performed
-# But can't guarentee that an increase in reverse rate is always acheivable
-# Want to consider case where it isn't and there is a tradeoff
-
+@time tradeoffs()
 
 # New stuff to write
 # Script to find parameters sets for each tradeoff

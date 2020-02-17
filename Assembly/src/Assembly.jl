3d1aed0693fc07640c52c97503a769e041a4423d
Jacob Cook <jc2017@ic.ac.uk>
2020-02-17 13:18:51 +0000

    Wrote out basic module

--- Assembly/src/Assembly.jl
+++ Assembly/src/Assembly.jl
@@ -1,5 +1,7 @@
 module Assembly
 
-greet() = print("Hello World!")
+# Include scripts that contain other functions
+include("MyPlots.jl") # This means I must always have a version of MyPlots.jl available
+
 
 end # module

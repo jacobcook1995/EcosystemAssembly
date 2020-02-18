ff30c8bc91bc136bcd1f50b14fe9e32ac6573fda
Jacob Cook <jc2017@ic.ac.uk>
2020-02-18 11:45:55 +0000

    Starting with the parameter definitions

--- Assembly/src/mars.jl
+++ Assembly/src/mars.jl
@@ -7,6 +7,19 @@ function vin(pref::Float64,conc::Float64)
     return(pref*conc)
 end
 
+# function to find rate of output of a particular resource, this matches vin as a linear function
+# wb is the value of the output resource, vins is 1D as only need inputs for specific microbe type
+# Da is the vector of transformations from resource a to other resources
+function vout(l::Array{Float64},wb::Float64,w::Array{Float64,1},vins::Array{Float64,1},Da::Array{Float64,1})
+    # Initialise vout to zero
+    vo = 0
+    # Loop over all resourses
+    for i = 1:length(l)
+        vo += (w[i]/wb)*Da[i]*l[i]*vins[i]
+    end
+    return(vo)
+end
+
 # function to find the energy obtained for growth from all resources for a particular cell
 function Jgrow(M::Int64,l::Array{Float64,1},vals::Array{Float64,1},vins::Array{Float64,1})
     Jg = 0
@@ -18,6 +31,8 @@ function Jgrow(M::Int64,l::Array{Float64,1},vals::Array{Float64,1},vins::Array{F
 end
 
 # function to implement the consumer resource dynamics
+# I currently provide vin and vout in order to avoid uneeded allocations => Is this sensible?
+# Also think that pop and concs need to be merged => Do this later if neccesary
 function dynamics(ps::Parameters,dxdt::Array{Float64,1},pop::Array{Float64,1},concs::Array{Float64,1},vins::Array{Float64,2},vouts::Array{Float64,2})
     # First find and store intake rates
     for j = 1:ps.M
@@ -25,21 +40,52 @@ function dynamics(ps::Parameters,dxdt::Array{Float64,1},pop::Array{Float64,1},co
             vins[i,j] = vin(ps.c[i,j],concs[j])
         end
     end
-    # Then use to find output rates, NEED TO PUT A FORMULA IN HERE
-
+    # Then use to find output rates
+    for j = 1:ps.M
+        for i = 1:ps.N
+            vouts[i,j] = vout(ps.l,ps.w[j],ps.w,vins[i,:],ps.D[j,:])
+        end
+    end
     # First consumer dynamics
     for i = 1:ps.N
         dxdt[i] = ps.g[i]*pop[i]*(Jgrow(ps.M,ps.l,ps.w,vins[i,:]) - ps.m[i])
     end
     # Then resource dynamics
     for i = ps.N+1:ps.N+ps.M
-        # fist add external supply of resource
-        dxdt[i] = ps.h[i-ps.N]
+        # fist add external supply of resource and decay
+        dxdt[i] = ps.κ[i-ps.N] - ps.δ[i-ps.N]
         # Then consider contribution by various microbes
         for j = 1:ps.N
-            dxdt[i] += pop[i]*(vouts[j,i] - vins[j,i])
+            dxdt[i] += pop[i]*(vouts[j,i-ps.N] - vins[j,i-ps.N])
         end
     end
     # Finally return the new dxdt values
     return(dxdt)
 end
+
+# function to run simulation of the Marsland model
+function simulate()
+    # Going to start with a small number of consumers and metabolities so that it runs fast, is easy to debug
+    N = 5
+    M = 5
+    # All metabolities have the same value for simplicity
+    w = ones(M)
+    # And all proportionality constants are the same for simplicity
+    g = ones(N)
+    # All but resource 1 is not supplied
+    κ = zeros(M)
+    κ[1] = 100.0
+    # Leakage fractions are assumed to be equal across all metabolities
+    li = 0.1
+    l = li*ones(M)
+    # Assume that all δ's are equal
+    δi = 1.0
+    δ = δi*ones(M)
+    # HOW ARE c, m, and D DETERMINED?
+    # D is from Dirichlet distribution => Should make a function for this
+    # c is a bit more fiddly => Needs a function
+    # m is fixed with a Guassian offset => Also needs to be a function
+    return(nothing)
+end
+
+@time simulate()
--- Assembly/src/parameters/marsParameters.jl
+++ Assembly/src/parameters/marsParameters.jl
@@ -1,6 +1,7 @@
 # The function and struct used in this script were adapted from code orginally written by Tom Clegg
 
-# I WILL NEED TO ALTER THE NAMES AND DEFINITIONS TO SUIT MY PURPOSES
+# Export Parameters so that it can be used elsewhere in my code
+export Parameters
 
 """
     Parameters(N::Int64,M::Int64,u::Array{Float64,2},m::Vector{Float64},g::Vector{Float64},l::Vector{Float64},h::Vector{Float64})
@@ -12,8 +13,10 @@ Type containing the parameters for a simuation.
 - `m::Vector{Float64}`: Vector of maintenance energy costs
 - `g::Vector{Float64}`: Vector of proportionality constants between growth and biomass
 - `l::Vector{Float64}`: Vector of leakage fraction of particular resources
-- `h::Vector{Float64}`: Vector of external resource supply rates
+- `κ::Vector{Float64}`: Vector of external resource supply rates
 - `w::Vector{Float64}`: Vector of resource energy values
+- `D::Array{Float64,2}`: Array of metabolic transistions
+- `δ::Vector{Float64}`: Vector of resource decay rates
 """
 struct Parameters
     N::Int64;
@@ -22,27 +25,34 @@ struct Parameters
     m::Vector{Float64}
     g::Vector{Float64}
     l::Vector{Float64}
-    h::Vector{Float64}
+    κ::Vector{Float64}
     w::Vector{Float64}
+    D::Array{Float64,2}
+    δ::Vector{Float64}
 end
 
 """
-    make_Parameters(N::Int64,M::Int64,c::Array{Float64,2},m::Vector{Float64},g::Vector{Float64},l::Vector{Float64},h::Vector{Float64},w::Vector{Float64})
+    make_Parameters(N::Int64,M::Int64,c::Array{Float64,2},m::Vector{Float64},g::Vector{Float64},l::Vector{Float64},κ::Vector{Float64},w::Vector{Float64},D::Array{Float64,2},δ::Vector{Float64})
 Helper function used internally. Takes values for parameters and returns a `Parameters`
 object. Also does checks internally to make sure the values are correct.
 """
-function make_Parameters(N::Int64,M::Int64,c::Array{Float64,2},m::Vector{Float64},g::Vector{Float64},l::Vector{Float64},h::Vector{Float64},w::Vector{Float64})
+function make_Parameters(N::Int64,M::Int64,c::Array{Float64,2},m::Vector{Float64},g::Vector{Float64},l::Vector{Float64},κ::Vector{Float64},w::Vector{Float64},D::Array{Float64,2},δ::Vector{Float64})
 
 # Use asserts to ensure that arrays are correct sizes
 @assert size(c) == (N,M) "Consumer preference array (c) is the wrong size"
 @assert length(m) == N "Vector of maintenance costs (m) is the wrong length"
-@assert length(g) == M "Vector of proportionality constants (g) vector is the wrong length"
+@assert length(g) == N "Vector of proportionality constants (g) vector is the wrong length"
 @assert length(l) == M "Vector of leakage fractions (l) is wrong length"
-@assert length(h) == M "Vector of external resource supplies (h) is the wrong length"
+@assert length(κ) == M "Vector of external resource supplies (κ) is the wrong length"
 @assert length(w) == M "Vector of resource values (w) is the wrong length "
+@assert size(D) == (M,M) "Metabolic matrix (D) is the wrong size"
+@assert length(δ) == M "Vector of decay rates (δ) is the wrong length"
 
 # Use asserts to avoid unphysical proportions
 @assert all(l .<= 1) "One or more l values are > 1"
+@assert all(l .>= 0) "One or more l values are negative"
+@assert all(sum(D,dims=2) .= 1) "One or more rows in D does not sum to 1"
+@assert all(δ .>= 0) "One or more decay rates in δ are negative"
 
-return(Parameters(N,M,c,m,g,l,h,w))
+return(Parameters(N,M,c,m,g,l,κ,w,D,δ))
 end

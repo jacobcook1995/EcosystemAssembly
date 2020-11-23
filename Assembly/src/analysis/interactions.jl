7bb10c3a5dc7c06bcdfc98ac5a9396c2d7ed344b
Jacob Cook <jc2017@ic.ac.uk>
2020-11-23 15:13:03 +0000

    Wrote function to read in data

--- Assembly/src/analysis/interactions.jl
+++ Assembly/src/analysis/interactions.jl
@@ -212,5 +212,71 @@ function quantify_ints()
     return(nothing)
 end
 
+# Function to analyse interaction data
+function analyse_ints()
+    # Check that sufficent arguments have been provided
+    if length(ARGS) < 4
+        error("Insufficent inputs provided (looking for 4)")
+    end
+    # Preallocate the variables I want to extract from the input
+    Rl = 0
+    Ru = 0
+    syn = true
+    rps = 0
+    # Check that all arguments can be converted to integers
+    try
+        Rl = parse(Int64,ARGS[1])
+        Ru = parse(Int64,ARGS[2])
+        syn = parse(Bool,ARGS[3])
+        rps = parse(Int64,ARGS[4])
+    catch e
+           error("need to provide 3 integers and a bool")
+    end
+    # Check that simulation type is valid
+    if Rl < 1
+        error("lower bound has to be at least one reaction")
+    end
+    if Ru < Rl
+        error("upper bound can't be lower than the lower bound")
+    end
+    # Check that number of simulations is greater than 0
+    if rps < 1
+        error("number of repeats can't be less than 1")
+    end
+    println("Compiled!")
+    for i = 1:rps
+        # Read in relevant files
+        pfile = "Data/$(Rl)-$(Ru)$(syn)/RedParasReacs$(Rl)-$(Ru)Syn$(syn)Run$(i).jld"
+        if ~isfile(pfile)
+            error("run $(i) is missing a parameter file")
+        end
+        ofile = "Data/$(Rl)-$(Ru)$(syn)/RedOutputReacs$(Rl)-$(Ru)Syn$(syn)Run$(i).jld"
+        if ~isfile(ofile)
+            error("run $(i) is missing an output file")
+        end
+        efile = "Data/$(Rl)-$(Ru)$(syn)/RedExtinctReacs$(Rl)-$(Ru)Syn$(syn)Run$(i).jld"
+        if ~isfile(efile)
+            error("run $(i) is missing an extinct file")
+        end
+        ifile = "Data/$(Rl)-$(Ru)$(syn)/IntsReacs$(Rl)-$(Ru)Syn$(syn)Run$(i).jld"
+        if ~isfile(ifile)
+            error("run $(i) is missing an interaction file")
+        end
+        # Basically just loading everything out as I'm not sure what I'll need
+        ps = load(pfile,"ps")
+        C = load(ofile,"C")
+        T = load(ofile,"T")
+        out = load(ofile,"out")
+        inf_out = load(ofile,"inf_out")
+        ded = load(efile,"ded")
+        Fatp = load(ifile,"Fatp")
+        frc = load(ifile,"frc")
+        ints = load(ifile,"ints")
+        in_str = load(ifile,"in_str")
+        net_in = load(ifile,"net_in")
+    end
+    return(nothing)
+end
+
 
 @time quantify_ints()

21ecf79911cb84405adcd09144fb9548efef0e01
Jacob Cook <jc2017@ic.ac.uk>
2021-05-06 10:01:27 +0100

    Added entropy production subplot

--- Assembly/src/figures/Fig2.jl
+++ Assembly/src/figures/Fig2.jl
@@ -6,6 +6,7 @@ using StatsBase
 using KernelDensity
 using StatsPlots
 using Plots.PlotMeasures
+using LsqFit
 import PyPlot
 
 # function to calculate the dissipation for an assembled ecosystem
@@ -49,11 +50,107 @@ function dissipation(ps::FullParameters,ms::Array{MicrobeP,1},out::Array{Float64
     return(dsp)
 end
 
+# Function to count the number of spikes in the entropy production trace
+function ent_sp_cnt(ps::FullParameters,C::Array{Float64,2},T::Array{Float64,1},Ns::Int64,
+                    out::Array{Float64,1},inf_out::Array{Float64,1},ded::Array{MicrobeP,1})
+    # Make new vector of microbes
+    ms = Array{MicrobeP,1}(undef,Ns)
+    # Setup counter
+    cnt = 0
+    # Loop over all microbes
+    for i = 1:Ns
+        # Check if it is a survivor
+        if C[end,i] != 0.0 && C[end,i] ∈ out
+            # If it is find and save it
+            ind = findfirst(x->x==C[end,i],out)
+            ms[i] = ps.mics[ind]
+        else
+            # Update counter
+            cnt += 1
+            # Use next element from ded vector
+            ms[i] = ded[cnt]
+        end
+    end
+    # Find final time
+    Tmax = T[end]
+    # container to store entropy production
+    ep = zeros(length(T))
+    # Calculate entropy production at each step
+    for i = 1:length(T)
+        # Calculate entropy production at each step
+        ep[i] = dissipation(ps,ms,C[i,:])
+    end
+    # container to store changes in entropy productions
+    dep = zeros(length(T))
+    # Find change between one point and the previous one
+    for i = 1:length(T)
+        if i > 1
+            dep[i] = ep[i] - ep[i-1]
+        else
+            dep[i] = ep[i]
+        end
+    end
+    # Find first peak, i.e. first time entropy production decays
+    pind = findfirst(x->x<0.0,dep) - 1
+    # Store in a vector
+    pinds = [pind]
+    # Counter for peaks
+    pc = 1
+    # Has the end of the trajectory been reached
+    nd = false
+    # Set inital offset
+    off = pind
+    # Now identify every peak
+    while nd == false
+        # Find first point where entropy production increases again
+        try
+            tind = findfirst(x->x>0.0,dep[(off+1):end]) - 1
+            # Add trough to offset
+            off += tind
+        # Error arises because we're at the end of the vector
+        catch e
+            # Set offset to maximum
+            off = length(T)
+            # and finish loop
+            nd = true
+        end
+        # Then find next peak based on this
+        try
+            pind = findfirst(x->x<0.0,dep[(off+1):end]) - 1
+        # Error arises because we're at the end of the vector
+        catch e
+            # Set final peak
+            pind = length(T) - off
+            # and finish loop
+            nd = true
+        end
+        # Check difference between peak and trough height
+        df = (abs(ep[(pind+off)]) - abs(ep[(off)]))/(abs(ep[(pind+off)]))
+        # If a greater than 1% change save
+        if df > 0.01
+            # Add new value to the vector
+            pinds = cat(pinds,pind+off,dims=1)
+            # Increment counter
+            pc += 1
+        end
+        # Finally add 'peak' to the offset, regardless of if it's a true peak or not
+        off += pind
+    end
+    # Check number of peaks is reasonable (can be wrong due to noisy trajectories)
+    if pc > 100
+        # If it's not set as a NaN
+        pc = NaN
+    end
+    return(pc)
+end
+
 function figure2(Rl::Int64,Ru::Int64,syn::Bool,Nr::Int64,Ns::Int64,en::String,Tf::Float64,rps::Int64)
     println("Compiled!")
     # Set intial number of concentrations, and preallocate the final ones
     nmi = ones(rps)
     nmf = zeros(rps)
+    # Preallocate number of entropy production spikes
+    pcs = zeros(rps)
     # Loop over all repeats to find substrate diversification
     for i = 1:rps
         # Read in relevant files
@@ -70,6 +167,35 @@ function figure2(Rl::Int64,Ru::Int64,syn::Bool,Nr::Int64,Ns::Int64,en::String,Tf
         inf_out = load(ofile,"inf_out")
         # Only want to count substrates
         nmf[i] = count(x->x>0.0,inf_out[(ps.N+1):(ps.N+ps.M-1)])
+    end
+    # Loop to count entropy production spikes
+    for i = 1:rps
+        # Read in specific files needed for the dynamics
+        pfile = "Data/$(Rl)-$(Ru)$(syn)$(Ns)$(en)/ParasReacs$(Rl)-$(Ru)Syn$(syn)Run$(i)Ns$(Ns).jld"
+        if ~isfile(pfile)
+            error("run $(Nr) is missing a parameter file")
+        end
+        ofile = "Data/$(Rl)-$(Ru)$(syn)$(Ns)$(en)/OutputReacs$(Rl)-$(Ru)Syn$(syn)Run$(i)Ns$(Ns).jld"
+        if ~isfile(ofile)
+            error("run $(Nr) is missing an output file")
+        end
+        ofile2 = "Data/$(Rl)-$(Ru)$(syn)$(Ns)$(en)/RedOutputReacs$(Rl)-$(Ru)Syn$(syn)Run$(i)Ns$(Ns).jld"
+        if ~isfile(ofile)
+            error("run $(Nr) is missing a 2nd output file")
+        end
+        efile = "Data/$(Rl)-$(Ru)$(syn)$(Ns)$(en)/ExtinctReacs$(Rl)-$(Ru)Syn$(syn)Run$(i)Ns$(Ns).jld"
+        if ~isfile(efile)
+            error("run $(Nr) is missing an extinct file")
+        end
+        # Read in relevant data
+        ps = load(pfile,"ps")
+        C = load(ofile,"C")
+        T = load(ofile,"T")
+        out = load(ofile,"out")
+        inf_out = load(ofile2,"inf_out")
+        ded = load(efile,"ded")
+        # Then run peak counting function
+        pcs[i] = ent_sp_cnt(ps,C,T,Ns,out,inf_out,ded)
     end
     # Read in specific files needed for the dynamics
     pfile = "Data/$(Rl)-$(Ru)$(syn)$(Ns)$(en)/ParasReacs$(Rl)-$(Ru)Syn$(syn)Run$(Nr)Ns$(Ns).jld"
@@ -169,6 +295,34 @@ function figure2(Rl::Int64,Ru::Int64,syn::Bool,Nr::Int64,Ns::Int64,en::String,Tf
             exT[i] = NaN
         end
     end
+    # Find indices of NaNs
+    nans = isnan.(pcs)
+    # Need to reduce data here to only include the relevant points
+    xdataT = pcs[.!nans]
+    ydataT = nmf[.!nans] .- 1
+    # Now calculate Pearson correlation coefficient
+    xbarT = sum(xdataT)/length(xdataT)
+    ybarT = sum(ydataT)/length(ydataT)
+    a = 0
+    b = 0
+    c = 0
+    for i = 1:length(xdataT)
+        a += (xdataT[i] - xbarT)*(ydataT[i] - ybarT)
+        b += (xdataT[i] - xbarT)^2
+        c += (ydataT[i] - ybarT)^2
+    end
+    r = a/sqrt(b*c)
+    println("Correlation between peaks and substrates: $(r)")
+    # Set model to fit the line to
+    @. model(x,p) = p[1] + p[2]*x
+    p0 = [0.0,1.0] # Initial values
+    # Fit model
+    fitT = curve_fit(model,xdataT,ydataT,p0)
+    # Extract values
+    yintT = coef(fitT)[1]
+    slopT = coef(fitT)[2]
+    println("Intercept = $(yintT)")
+    println("Slope = $(slopT)")
     # Set line width (plots defaults to 1)
     wdt = 2
     # Now move onto plotting
@@ -273,6 +427,17 @@ function figure2(Rl::Int64,Ru::Int64,syn::Bool,Nr::Int64,Ns::Int64,en::String,Tf
             plot!(p4,[exT[i];exT[i]],[-0.01;0.01],color=wongc[i],style=:solid,lw=wdt,label="")
         end
     end
+    # Define box for inset here
+    box = (1,bbox(0.65,0.20,0.325,0.325,:bottom,:left))
+    # Plot scatter points as an inset
+    scatter!(p4,xdataT,ydataT,ms=6,color=:black,label="",inset_subplots=box,subplot=2)
+    # Set range of x values to plot for
+    xran = 0.0:1.0:25.0
+    # and then plot best fit line
+    plot!(p4[2],xran,model(xran,[yintT,slopT]),label="",color=:red,lw=wdt)
+    # Set labels, fontsize, etc
+    plot!(p4[2],guidefontsize=12,legendfontsize=12,tickfontsize=9,grid=false)
+    plot!(p4[2],xlabel="Entropy production peaks",ylabel="New metabolites",ylim=(0,25),xlim=(0,25))
     savefig(p4,"Output/Fig2/entp.png")
     # Now want to make a plot incorperating all four previous plots
     pt = plot(p1,p3,p2,p4,layout=(4,1),size=(900,1600),margin=5mm,grid=false)

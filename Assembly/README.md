2bc6b0bddbea63432a1db6b71483fe76026bb441
Jacob Cook <jc2017@ic.ac.uk>
2021-08-17 13:53:24 +0100

    Made interaction plots

--- Assembly/README.md
+++ Assembly/README.md
@@ -10,6 +10,7 @@ Made a package to contain code used for modelling microbial ecosystem assembly w
 7. Figure 4 -> src/figures/Fig4.jl
 8. Figure 5 -> src/figures/Fig5.jl
 9. Supplementary figures -> src/figures/SI_figures.jl
+10. Further supplementary figures -> src/figures/vis_ints.jl
 ## Full procedure for generating the data for the paper
 All simulations are carried out using the form_comm.jl file in the analysis file.
 This file generates a parameter set using the make_full.jl and fullParameters.jl scripts (in the parameters folder).
--- Assembly/src/parameters/make_full.jl
+++ Assembly/src/parameters/make_full.jl
@@ -1,5 +1,5 @@
 # Script that makes the parameters needed for simulation of the full proteome model
-export initialise
+export initialise, initialise_syn_trio, initialise_pol_duo
 
 # function to randomly choose the reactions that a specific microbe can make use of
  function choose_reactions(O::Int64,Rl::Int64,Ru::Int64)
@@ -151,6 +151,179 @@ function initialise(N::Int64,M::Int64,O::Int64,Rl::Int64,Ru::Int64,kc::Float64,K
         ϕP = ϕP/sum(ϕP)
         # Find corresponding η's for these reactions
         η = choose_η_mix(reacs,Reacs,T,syn)
+        # Can finally generate microbe
+        mics[i] = make_MicrobeP(MC,γm,ρ,Kγ,Pb,d,ϕH,KΩ,fd,R,Reacs,η,kcs,KSs,krs,n,ϕP)
+    end
+    # Now make the parameter set
+    ps = make_FullParameters(N,M,O,T,κ,δ,reacs,mics)
+    return(ps)
+end
+
+# function to generate set of three strains that interact thermodynamically
+function initialise_syn_trio(kc::Float64,KS::Float64,kr::Float64)
+    # Trio of strains
+    N = 3
+    # Three metabolites
+    M = 3
+    # Correspondingly 3 reactions
+    O = 3
+    # One reaction per strain
+    R = 1
+    # Assume that temperature T is constant at 20°C
+    T = 293.15
+    # Cell mass is taken from Bremer H, Dennis P (1996) Modulation of chemical
+    # composition and other parameters of the cell by growth rate (Book chapter).
+    MC = 10^8
+    # Elongation rate also taken from Bremer (1996), convert from minutes to seconds
+    γm = 1260.0/60.0
+    # Now preallocate protein masses
+    n = zeros(Int64,3)
+    # ribosome mass taken from Keseler IM, et al. (2011)
+    n[1] = 7459
+    # Other protein mass averaged from Brandt F, et al. (2009)
+    n[2:3] .= 300
+    # Need to think carefully about what a reasonable parameter value is here
+    # This has a large impact on the fraction, so should be careful with this one
+    d = 6.0e-5
+    # The number of ATP per translation step, including the cost of amino acid sythesis
+    # This figure is taken from Lynch and Marinov 2015
+    ρ = 29.0
+    # Back to old arbitary figures, think this might be the best route
+    Kγ = 5e8
+    # The proportion of ribosomes bound is taken from Underwood et al to be 70%
+    Pb = 0.7
+    # Housekeeping fraction is taken from Scott et al. 2010
+    ϕH = 0.45
+    # Back to old arbitary figures, think this might be the best route
+    KΩ = 1e9
+    # Number of doublings required to dilute to 1%
+    fd = log(100)/log(2)
+    # From Posfai et al (2017) dilution rate 0.21 per hour
+    δ = 6.0e-5*ones(M) # Metabolite dilution rate
+    # Human blood glucose is approximatly 5.5 m mol per litre (wikipedia)
+    # Sensible order of magnitude to aim for set κ/δ = 5.5e-3
+    κ = zeros(M)
+    # All but resource 1 is not supplied
+    κ[1] = 3.3e-7 # Metabolite supply rate
+    # Chosen so that 100 steps yields slightly more free energy than respiring glucose
+    μrange = 5e6*(M/25)
+    # Generate fixed set of reactions
+    RP, ΔG = fix_reactions(O,M,μrange,T)
+    # Preallocate vector of reactions
+    reacs = Array{Reaction,1}(undef,O)
+    for i = 1:O
+        reacs[i] = make_Reaction(i,RP[i,1],RP[i,2],ΔG[i])
+    end
+    # Preallocate vector of microbes
+    mics = Array{MicrobeP,1}(undef,N)
+    # Then construct microbes
+    for i = 1:N
+        # Choose specific reactions and eta values for specific microbes
+        if i == 1
+            Reacs = [1]
+            η = [3.0]
+        elseif i == 2
+            Reacs = [1]
+            η = [3.85]
+        elseif i == 3
+            Reacs = [3]
+            η = [3.5]
+        else
+            error("too many strains provided, need a trio")
+        end
+        # Make vectors of the (fixed) kinetic parameters
+        kcs = [kc]
+        KSs = [KS]
+        krs = [kr]
+        # Reactions given random proportional weightings, done this in the simplest way possible
+        ϕP = rand(R)
+        ϕP = ϕP/sum(ϕP)
+        # Can finally generate microbe
+        mics[i] = make_MicrobeP(MC,γm,ρ,Kγ,Pb,d,ϕH,KΩ,fd,R,Reacs,η,kcs,KSs,krs,n,ϕP)
+    end
+    # Now make the parameter set
+    ps = make_FullParameters(N,M,O,T,κ,δ,reacs,mics)
+    return(ps)
+end
+
+# function to generate set of three strains that interact thermodynamically
+function initialise_pol_duo(kc::Float64,KS::Float64,kr::Float64)
+    # Duo of strains
+    N = 2
+    # Three metabolites
+    M = 3
+    # Correspondingly 3 reactions
+    O = 3
+    # One reaction per strain
+    R = 1
+    # Assume that temperature T is constant at 20°C
+    T = 293.15
+    # Cell mass is taken from Bremer H, Dennis P (1996) Modulation of chemical
+    # composition and other parameters of the cell by growth rate (Book chapter).
+    MC = 10^8
+    # Elongation rate also taken from Bremer (1996), convert from minutes to seconds
+    γm = 1260.0/60.0
+    # Now preallocate protein masses
+    n = zeros(Int64,3)
+    # ribosome mass taken from Keseler IM, et al. (2011)
+    n[1] = 7459
+    # Other protein mass averaged from Brandt F, et al. (2009)
+    n[2:3] .= 300
+    # Need to think carefully about what a reasonable parameter value is here
+    # This has a large impact on the fraction, so should be careful with this one
+    d = 6.0e-5
+    # The number of ATP per translation step, including the cost of amino acid sythesis
+    # This figure is taken from Lynch and Marinov 2015
+    ρ = 29.0
+    # Back to old arbitary figures, think this might be the best route
+    Kγ = 5e8
+    # The proportion of ribosomes bound is taken from Underwood et al to be 70%
+    Pb = 0.7
+    # Housekeeping fraction is taken from Scott et al. 2010
+    ϕH = 0.45
+    # Back to old arbitary figures, think this might be the best route
+    KΩ = 1e9
+    # Number of doublings required to dilute to 1%
+    fd = log(100)/log(2)
+    # From Posfai et al (2017) dilution rate 0.21 per hour
+    δ = 6.0e-5*ones(M) # Metabolite dilution rate
+    # Human blood glucose is approximatly 5.5 m mol per litre (wikipedia)
+    # Sensible order of magnitude to aim for set κ/δ = 5.5e-3
+    κ = zeros(M)
+    # Supply both metabolites now
+    κ[1] = 3.3e-7 # Metabolite supply rate
+    κ[2] = 3.3e-7
+    # Chosen so that 100 steps yields slightly more free energy than respiring glucose
+    μrange = 5e6*(M/25)
+    # Generate fixed set of reactions
+    RP, ΔG = fix_reactions(O,M,μrange,T)
+    # Preallocate vector of reactions
+    reacs = Array{Reaction,1}(undef,O)
+    for i = 1:O
+        reacs[i] = make_Reaction(i,RP[i,1],RP[i,2],ΔG[i])
+    end
+    # Preallocate vector of microbes
+    mics = Array{MicrobeP,1}(undef,N)
+    # Then construct microbes
+    for i = 1:N
+        # Choose specific reactions and eta values for specific microbes
+        if i == 1
+            Reacs = [3]
+            # 3.85 too low
+            η = [4.00]
+        elseif i == 2
+            Reacs = [2]
+            η = [6.00]
+        else
+            error("too many strains provided, need a duo")
+        end
+        # Make vectors of the (fixed) kinetic parameters
+        kcs = [kc]
+        KSs = [KS]
+        krs = [kr]
+        # Reactions given random proportional weightings, done this in the simplest way possible
+        ϕP = rand(R)
+        ϕP = ϕP/sum(ϕP)
         # Can finally generate microbe
         mics[i] = make_MicrobeP(MC,γm,ρ,Kγ,Pb,d,ϕH,KΩ,fd,R,Reacs,η,kcs,KSs,krs,n,ϕP)
     end

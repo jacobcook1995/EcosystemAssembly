2b4f7ca995a5e362d6869653b734f7f5370a9307
Jacob Cook <jc2017@ic.ac.uk>
2021-09-15 09:23:40 +0100

    Made new figure 4

--- TradeOff/src/figures/Fig4.jl
+++ TradeOff/src/figures/Fig4.jl
@@ -3,10 +3,26 @@ using TradeOff
 using Plots
 using JLD
 using ColorSchemes
+using LaTeXStrings
 using Plots.PlotMeasures
 import PyPlot
 
-function figure4(rps::Int64,ims::Int64,sim_type::Int64)
+function sub_prob(M::Int64,R::Int64,subs::Float64)
+    # Check that final waste product hasn't been generated
+    if subs >= M - 1
+        no_sub = M - 1
+    # Also check that it hasn't somehow gone negative
+    elseif subs <= 0.0
+        no_sub = 0.0
+    else
+        no_sub = subs
+    end
+    # Calculate probability
+    P = 1 - (1-(no_sub)/(M-1))^R
+    return(P)
+end
+
+function figure4(rps::Int64,ims::Int64,sim_type::Int64,sim_type2::Int64)
     println("Compiled")
     # Extract other simulation parameters from the function
     Np, Nt, M, d, μrange = sim_paras(sim_type)
@@ -15,80 +31,143 @@ function figure4(rps::Int64,ims::Int64,sim_type::Int64)
     if ~isfile(pfile)
         error("$(ims) immigrations run $(rN) is missing a parameter file")
     end
-    sfile = "Output/$(Np)Pools$(M)Metabolites$(Nt)Speciesd=$(d)u=$(μrange)/SnapDataStats$(ims)Ims.jld"
+    # Find file name to load in
+    sfile = "Output/$(Np)Pools$(M)Metabolites$(Nt)Speciesd=$(d)u=$(μrange)/RunStats$(ims)Ims.jld"
+    # Check it actually exists
     if ~isfile(sfile)
-        error("$(ims) immigrations run $(rN) is missing an output file")
+        error("missing stats file for $(ims) immigrations simulations")
     end
     # Read in relevant data
     ps = load(pfile,"ps")
-    # Save growth probabilities
-    gp = load(sfile,"gp")
-    mn_stb = load(sfile,"mn_stb")
-    mn_inc = load(sfile,"mn_inc")
-    mn_dec = load(sfile,"mn_dec")
-    sd_stb = load(sfile,"sd_stb")
-    sd_inc = load(sfile,"sd_inc")
-    sd_dec = load(sfile,"sd_dec")
-    st_r = load(sfile,"st_r")
-    s_times = load(sfile,"times")
-    # Find file name to load in
-    tfile = "Output/$(Np)Pools$(M)Metabolites$(Nt)Speciesd=$(d)u=$(μrange)/RunStats$(ims)Ims.jld"
-    # Check it actually exists
-    if ~isfile(tfile)
-        error("missing stats file for $(ims) immigrations simulations")
-    end
     # Now load out the times, and number of trajectories
-    t_times = load(tfile,"times")
-    no_sims = load(tfile,"no_sims")
-    no_via = load(tfile,"no_via")
+    times = load(sfile,"times")
+    no_sims = load(sfile,"no_sims")
+    no_via = load(sfile,"no_via")
+    no_rs = load(sfile,"no_rs")
     # Load in averages
-    mn_svt = load(tfile,"mn_svt")
-    mn_tsvt = load(tfile,"mn_tsvt")
+    mn_sbs = load(sfile,"mn_sbs")
+    mn_via_R = load(sfile,"mn_via_R")
+    mn_ηs_R = load(sfile,"mn_ηs_R")
+    mn_KS_R = load(sfile,"mn_KS_R")
     # Load in standard deviations
-    sd_svt = load(tfile,"sd_svt")
-    sd_tsvt = load(tfile,"sd_tsvt")
+    sd_sbs = load(sfile,"sd_sbs")
+    sd_via_R = load(sfile,"sd_via_R")
+    sd_ηs_R = load(sfile,"sd_ηs_R")
+    sd_KS_R = load(sfile,"sd_KS_R")
+    # Preallocate standard errors
+    se_via_R = zeros(size(sd_via_R))
+    se_ηs_R = zeros(size(sd_ηs_R))
+    se_KS_R = zeros(size(sd_KS_R))
+    # Calculate standard errors from this
+    se_sbs = sd_sbs./sqrt.(no_sims)
+    # Calculation (slightly) different in the viable case
+    for i = 1:size(sd_via_R,1)
+        se_via_R[i,:] = sd_via_R[i,:]./sqrt.(no_via)
+        se_ηs_R[i,:] = sd_ηs_R[i,:]./sqrt.(no_rs[i,:])
+        se_KS_R[i,:] = sd_KS_R[i,:]./sqrt.(no_rs[i,:])
+    end
+    # Preallocate probabilites
+    mn_Ps = zeros(size(mn_via_R))
+    up_Ps = zeros(size(mn_via_R))
+    dw_Ps = zeros(size(mn_via_R))
+    # Loop over, calculating the probability at each step
+    for i = 1:size(mn_Ps,1)
+        for j = 1:size(mn_Ps,2)
+            # Calculate mean probability
+            mn_Ps[i,j] = sub_prob(M,i,mn_sbs[j])
+            # And also upper and lower bound
+            up_Ps[i,j] = sub_prob(M,i,mn_sbs[j]+se_sbs[j]) .- mn_Ps[i,j]
+            dw_Ps[i,j] = mn_Ps[i,j] .- sub_prob(M,i,mn_sbs[j]-se_sbs[j])
+        end
+    end
     println("Data read in")
     # Check if directory exists and if not make it
     if ~isdir("Output/Fig4")
         mkdir("Output/Fig4")
     end
-    # Calculate standard errors
-    se_stb = sd_stb./sqrt.(st_r)
-    se_inc = sd_inc./sqrt.(st_r)
-    se_dec = sd_dec./sqrt.(st_r)
-    se_svt = sd_svt./sqrt.(no_sims)
-    se_tsvt = sd_tsvt./sqrt.(no_sims)
     # Setup plotting
     pyplot(dpi=200)
     # Load in color scheme
-    a = ColorSchemes.tab10.colors
-    # Plot total survivors
-    p1 = plot(xlabel="Time (s)",ylabel="Number of strains",xlim=(-Inf,5e7),legend=:topleft)
-    plot!(p1,t_times,mn_svt,ribbon=se_svt,label="Total",color=a[1])
-    plot!(p1,t_times,mn_tsvt,ribbon=se_tsvt,label="Viable",color=a[2])
+    a = ColorSchemes.sunset.colors
+    # Plot basic trade-off first
+    p1 = plot(xlabel="Time (s)",ylabel="Number of strains",xlim=(-Inf,5e7))
+    plot!(p1,title="High substrate free-energy case",legend=:bottomright)
+    plot!(p1,times,mn_via_R[1,:],ribbon=se_via_R[1,:],label="R=1",color=a[1])
+    plot!(p1,times,mn_via_R[3,:],ribbon=se_via_R[3,:],label="R=3",color=a[2])
+    plot!(p1,times,mn_via_R[5,:],ribbon=se_via_R[5,:],label="R=5",color=a[3])
+    plot!(p1,times,mn_via_R[7,:],ribbon=se_via_R[7,:],label="R=7",color=a[4])
     # Add annotation
-    px, py = annpos([0.0;5e7],[0.0;35.0],0.05,0.0)
+    px, py = annpos([0.0;5e7],[0.0;7.6],0.05,0.0)
     annotate!(p1,px,py,text("A",17,:black))
-    # Twin the x-axis
-    pt = twinx(p1)
-    # Ensure same limits are used
-    plot!(xlim=(-Inf,5e7))
-    # Then plot the proportion feasible
-    p2 = plot!(pt,s_times,gp,label="Feasibility",color=a[3],legend=:topright,ylabel="Proportion of invaders that are feasible")
-    savefig(p2,"Output/Fig4/Strains.png")
-    # Then plot various means
-    p3 = plot(xlabel="Time (s)",ylabel="Number of strains",xlim=(-Inf,5e7))
-    plot!(p3,s_times,mn_stb,ribbon=se_stb,label="Stable",color=a[4])
-    plot!(p3,s_times,mn_inc,ribbon=se_inc,label="Growing",color=a[5])
-    plot!(p3,s_times,mn_dec,ribbon=se_dec,label="Declining",color=a[6])
+    savefig(p1,"Output/Fig4/AvViaReacsTime.png")
+    # Now do probability plot
+    p2 = plot(xlabel="Time (s)",ylabel="Probability of no usable substrate",xlim=(-Inf,5e7))
+    plot!(p2,title="Chance of species finding no usable substrates",legend=false)
+    plot!(p2,times,1 .-mn_Ps[1,:],ribbon=(up_Ps[1,:],dw_Ps[1,:]),label="R=1",color=a[1])
+    plot!(p2,times,1 .-mn_Ps[3,:],ribbon=(up_Ps[3,:],dw_Ps[3,:]),label="R=3",color=a[2])
+    plot!(p2,times,1 .-mn_Ps[5,:],ribbon=(up_Ps[5,:],dw_Ps[5,:]),label="R=5",color=a[3])
+    plot!(p2,times,1 .-mn_Ps[7,:],ribbon=(up_Ps[7,:],dw_Ps[7,:]),label="R=7",color=a[4])
     # Add annotation
-    px, py = annpos([0.0;5e7],[0.0;21.0],0.05,0.0)
-    annotate!(p3,px,py,text("B",17,:black))
-    savefig(p3,"Output/Fig4/GrowvsDecl.png")
-    # Now want to make a plot incorperating both previous plots
-    pt = plot(p2,p3,layout=(2,1),size=(750,800),margin=5.0mm,rightmargin=20mm)
+    px, py = annpos([0.0;5e7],[0.0;1.1],0.05,0.0)
+    annotate!(p2,px,py,text("B",17,:black))
+    savefig(p2,"Output/Fig4/ProbSubTime.png")
+    # Plot trade-off for η
+    p3 = plot(xlabel="Time (s)",ylabel="Average eta value",xlim=(-Inf,5e7))
+    plot!(p3,title="Variation of key parameters",legend=false)
+    plot!(p3,times,mn_ηs_R[1,:],ribbon=se_ηs_R[1,:],label="R=1",color=a[1])
+    plot!(p3,times,mn_ηs_R[3,:],ribbon=se_ηs_R[3,:],label="R=3",color=a[2])
+    plot!(p3,times,mn_ηs_R[5,:],ribbon=se_ηs_R[5,:],label="R=5",color=a[3])
+    plot!(p3,times,mn_ηs_R[7,:],ribbon=se_ηs_R[7,:],label="R=7",color=a[4])
+    # Add annotation
+    px, py = annpos([0.0;5e7],[1.2;3.95],0.05,0.0)
+    annotate!(p3,px,py,text("C",17,:black))
+    # Define box for inset here
+    box = (1,bbox(0.4,0.15,0.4,0.3,:bottom,:left))
+    Ks = L"K_S"
+    e7 = L"10^7"
+    em3 = L"10^{-3}"
+    # Plot other trade-off into the inset
+    plot!(p3,times/1e7,mn_KS_R[1,:]*1000.0,ribbon=se_KS_R[1,:]*1000.0,color=a[1],label="",inset_subplots=box,subplot=2)
+    plot!(p3,times/1e7,mn_KS_R[3,:]*1000.0,ribbon=se_KS_R[3,:]*1000.0,color=a[2],label="",subplot=2)
+    plot!(p3,times/1e7,mn_KS_R[5,:]*1000.0,ribbon=se_KS_R[5,:]*1000.0,color=a[3],label="",subplot=2)
+    plot!(p3,times/1e7,mn_KS_R[7,:]*1000.0,ribbon=se_KS_R[7,:]*1000.0,color=a[4],label="",subplot=2)
+    plot!(p3,xlabel="Time ($(e7) s)",ylabel="$(Ks) ($em3)",xlim=(-Inf,5.0),grid=false,subplot=2)
+    savefig(p3,"Output/Fig4/AvEtaperReacTime.png")
+    # Extract simulation parameters for the other case from the function
+    Np, Nt, M, d, μrange = sim_paras(sim_type2)
+    # Find file name to load in
+    sfile = "Output/$(Np)Pools$(M)Metabolites$(Nt)Speciesd=$(d)u=$(μrange)/RunStats$(ims)Ims.jld"
+    # Check it actually exists
+    if ~isfile(sfile)
+        error("missing stats file for $(ims) immigrations simulations")
+    end
+    # Now load out the times, and number of trajectories
+    times = load(sfile,"times")
+    no_via = load(sfile,"no_via")
+    # Load in averages
+    mn_via_R = load(sfile,"mn_via_R")
+    # Load in standard deviations
+    sd_via_R = load(sfile,"sd_via_R")
+    # Preallocate standard errors
+    se_via_R = zeros(size(sd_via_R))
+    # Calculation (slightly) different in the viable case
+    for i = 1:size(sd_via_R,1)
+        se_via_R[i,:] = sd_via_R[i,:]./sqrt.(no_via)
+    end
+    p4 = plot(xlabel="Time (s)",ylabel="Number of strains",xlim=(-Inf,5e7))
+    plot!(p4,title="Low substrate free-energy case",legend=false)
+    plot!(p4,times,mn_via_R[1,:],ribbon=se_via_R[1,:],label="R=1",color=a[1])
+    plot!(p4,times,mn_via_R[3,:],ribbon=se_via_R[3,:],label="R=3",color=a[2])
+    plot!(p4,times,mn_via_R[5,:],ribbon=se_via_R[5,:],label="R=5",color=a[3])
+    plot!(p4,times,mn_via_R[7,:],ribbon=se_via_R[7,:],label="R=7",color=a[4])
+    # Add annotation
+    px, py = annpos([0.0;5e7],[0.0;9.25],0.05,0.0)
+    annotate!(p4,px,py,text("D",17,:black))
+    savefig(p4,"Output/Fig4/LowFreeEnergy.png")
+    # Plot all graphs as a single figure
+    pt = plot(p1,p3,p2,p4,layout=4,size=(1200,800),margin=5.0mm)
     savefig(pt,"Output/Fig4/figure4.png")
     return(nothing)
 end
 
-@time figure4(250,500,1)
+@time figure4(250,500,1,2)

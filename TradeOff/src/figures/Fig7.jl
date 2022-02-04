7b4acc35f03757c1f9e25005aa933bc73af61d1e
Jacob Cook <jc2017@ic.ac.uk>
2022-02-04 11:49:47 +0000

    Made acceptable version of figure 7

--- TradeOff/src/figures/Fig7.jl
+++ TradeOff/src/figures/Fig7.jl
@@ -7,7 +7,6 @@ using LaTeXStrings
 using Plots.PlotMeasures
 import PyPlot
 
-# WORK OUT IF THIS, AND OTHER STUFF IS ACTUALLY NEEDED
 function sub_prob(M::Int64,R::Int64,subs::Float64)
     # Check that final waste product hasn't been generated
     if subs >= M - 1
@@ -49,49 +48,28 @@ function figure7(rps::Int64)
     times = load(sfile,"times")
     no_sims = load(sfile,"no_sims")
     no_via = load(sfile,"no_via")
-    no_rs = load(sfile,"no_rs")
     # Load in averages
     mn_sbs = load(sfile,"mn_sbs")
     mn_via_R = load(sfile,"mn_via_R")
-    mn_ηs_R = load(sfile,"mn_ηs_R")
-    mn_KS_R = load(sfile,"mn_KS_R")
     mn_via_ω = load(sfile,"mn_via_ω")
     mn_via_ϕR = load(sfile,"mn_via_ϕR")
     mn_via_η = load(sfile,"mn_via_η")
-    mn_fr_ΔG = load(sfile,"mn_fr_ΔG")
-    mn_fr_ΔG1 = load(sfile,"mn_fr_ΔG1")
-    mn_fr_ΔG2 = load(sfile,"mn_fr_ΔG2")
-    mn_av_steps = load(sfile,"mn_av_steps")
     # Load in standard deviations
     sd_sbs = load(sfile,"sd_sbs")
     sd_via_R = load(sfile,"sd_via_R")
-    sd_ηs_R = load(sfile,"sd_ηs_R")
-    sd_KS_R = load(sfile,"sd_KS_R")
     sd_via_ω = load(sfile,"sd_via_ω")
     sd_via_ϕR = load(sfile,"sd_via_ϕR")
     sd_via_η = load(sfile,"sd_via_η")
-    sd_fr_ΔG = load(sfile,"sd_fr_ΔG")
-    sd_fr_ΔG1 = load(sfile,"sd_fr_ΔG1")
-    sd_fr_ΔG2 = load(sfile,"sd_fr_ΔG2")
-    sd_av_steps = load(sfile,"sd_av_steps")
     # Preallocate standard errors
     se_via_R = zeros(size(sd_via_R))
-    se_ηs_R = zeros(size(sd_ηs_R))
-    se_KS_R = zeros(size(sd_KS_R))
     # Calculate standard errors from this
     se_sbs = sd_sbs./sqrt.(no_sims)
     se_via_ω = sd_via_ω./sqrt.(no_via)
     se_via_ϕR = sd_via_ϕR./sqrt.(no_via)
     se_via_η = sd_via_η./sqrt.(no_via)
-    se_fr_ΔG = sd_fr_ΔG./sqrt.(no_via)
-    se_fr_ΔG1 = sd_fr_ΔG1./sqrt.(no_via)
-    se_fr_ΔG2 = sd_fr_ΔG2./sqrt.(no_via)
-    se_av_steps = sd_av_steps./sqrt.(no_via)
     # Calculation (slightly) different in the viable case
     for i = 1:size(sd_via_R,1)
         se_via_R[i,:] = sd_via_R[i,:]./sqrt.(no_via)
-        se_ηs_R[i,:] = sd_ηs_R[i,:]./sqrt.(no_rs[i,:])
-        se_KS_R[i,:] = sd_KS_R[i,:]./sqrt.(no_rs[i,:])
     end
     # Preallocate probabilites
     mn_Ps = zeros(size(mn_via_R))
@@ -118,13 +96,13 @@ function figure7(rps::Int64)
     a = ColorSchemes.sunset.colors
     # Plot basic trade-off first
     p1 = plot(xlabel="Time (s)",ylabel="Number of species",xlim=(-Inf,2.5e6))
-    plot!(p1,title="High substrate free-energy case",legend=:bottomright)
+    plot!(p1,title="Number of reactions with time",legend=:bottomright)
     plot!(p1,times,mn_via_R[1,:],ribbon=se_via_R[1,:],label="R=1",color=a[1])
     plot!(p1,times,mn_via_R[3,:],ribbon=se_via_R[3,:],label="R=3",color=a[2])
     plot!(p1,times,mn_via_R[5,:],ribbon=se_via_R[5,:],label="R=5",color=a[3])
     plot!(p1,times,mn_via_R[7,:],ribbon=se_via_R[7,:],label="R=7",color=a[4])
     # Add annotation
-    px, py = annpos([0.0;5e7],[0.0;7.6],0.05,0.0)
+    px, py = annpos([0.0;2.5e6],[0.0;10.0],0.05,0.0)
     annotate!(p1,px,py,text("A",17,:black))
     savefig(p1,"Output/Fig7/AvViaReacsTime.png")
     # Now do probability plot
@@ -135,7 +113,7 @@ function figure7(rps::Int64)
     plot!(p2,times,1 .-mn_Ps[5,:],ribbon=(up_Ps[5,:],dw_Ps[5,:]),label="R=5",color=a[3])
     plot!(p2,times,1 .-mn_Ps[7,:],ribbon=(up_Ps[7,:],dw_Ps[7,:]),label="R=7",color=a[4])
     # Add annotation
-    px, py = annpos([0.0;5e7],[0.0;1.1],0.05,0.0)
+    px, py = annpos([0.0;2.5e6],[0.0;1.1],0.05,0.0)
     annotate!(p2,px,py,text("B",17,:black))
     savefig(p2,"Output/Fig7/ProbSubTime.png")
     # Load in color scheme
@@ -146,46 +124,29 @@ function figure7(rps::Int64)
     e6 = L"10^6"
     # Make plot objects
     p3 = plot(xlabel="Time (s)",xlim=(-Inf,2.5e6),ylim=(0.5,0.65),legend=:topleft)
-    plot!(p3,title="Variation with free energy",ylabel="Maximum ribsome fraction factor ($(L"\omega"))")
+    plot!(p3,title="Ribosome dynamics",ylabel="Maximum ribsome fraction factor ($(L"\omega"))")
     # Add inset box to plot other trade-off into
     plot!(p3,xlabel="Time ($(e6) s)",ylabel=L"\phi_R",inset_subplots=box,subplot=2,grid=false,legend=false)
     plot!(p3,subplot=2,xlim=(-Inf,2.5),ylim=(0.05,0.3),legend=false)
     # Then actually plot the relevant data
     plot!(p3,times,mn_via_ω,ribbon=se_via_ω,label="",color=a[1])
     plot!(p3,times/1e6,mn_via_ϕR,ribbon=se_via_ϕR,color=a[1],subplot=2)
+    # Add annotation
+    px, py = annpos([0.0;2.5e6],[0.0;0.654],0.1,0.0)
+    annotate!(p3,px,py,text("C",17,:black))
     savefig(p3,"Output/Fig7/omega_with_t.png")
-    # Placeholder
-    p4 = plot()
+    # Make plot objects
+    p4 = plot(xlabel="Times (s)",xlim=(-Inf,2.5e6),legend=:right,ylabel=L"\eta",title="ATP yield with time")
+    # Plot the data to the relevant plot objects
+    plot!(p4,times,mn_via_η,ribbon=se_via_η,color=a[1],label="")
+    # Add annotation
+    px, py = annpos([0.0;2.5e6],[0.0;3.385],0.05,0.0)
+    annotate!(p4,px,py,text("D",17,:black))
+    # Save figures to this directory
+    savefig(p4,"Output/Fig7/Eta.png")
     # Plot all graphs as a single figure
     pt = plot(p1,p3,p2,p4,layout=4,size=(1200,800),margin=5.0mm)
     savefig(pt,"Output/Fig7/figure7.png")
-    # Load in color schemes
-    a = ColorSchemes.Dark2_4.colors
-    # Extract specific 4 colors from a color scheme
-    bt = ColorSchemes.tab10.colors
-    b = [bt[10];bt[9];bt[7];bt[5]]
-    # Make plot objects
-    p1 = plot(xlabel="Times (s)",xlim=(-Inf,2.5e6),legend=:right,ylabel=L"\eta",title="ATP yield")
-    p2 = plot(xlabel="Times (s)",xlim=(-Inf,2.5e6),ylim=(0.55,Inf),legend=false)
-    plot!(p2,ylabel="Fraction of free-energy transduced",title="Average reaction efficiency")
-    p3 = plot(xlabel="Times (s)",xlim=(-Inf,2.5e6),ylim=(0.5,Inf),legend=:bottomright)
-    plot!(p3,ylabel="Fraction of free-energy transduced",title="Reaction efficency by reaction type")
-    p4 = plot(xlabel="Times (s)",xlim=(-Inf,2.5e6),ylim=(0.4,Inf),legend=false)
-    plot!(p4,ylabel="Proportion of 2-step reactions",title="Relative frequency of reaction types")
-    # Plot the data to the relevant plot objects
-    plot!(p1,times,mn_via_η,ribbon=se_via_η,color=a[1],label="")
-    plot!(p2,times,mn_fr_ΔG,ribbon=se_fr_ΔG,color=a[1])
-    plot!(p3,times,mn_fr_ΔG1,ribbon=se_fr_ΔG1,color=b[1],label="1-step")
-    plot!(p3,times,mn_fr_ΔG2,ribbon=se_fr_ΔG2,color=b[3],label="2-step")
-    plot!(p4,times,mn_av_steps.-1,ribbon=se_av_steps,color=a[1])
-    # Save figures to this directory
-    savefig(p1,"Output/Fig7/Eta.png")
-    savefig(p2,"Output/Fig7/Efficency.png")
-    savefig(p3,"Output/Fig7/CompEfficencies.png")
-    savefig(p4,"Output/Fig7/Steps.png")
-    # Plot all graphs as a single figure
-    pt = plot(p1,p2,p3,p4,layout=(2,2),size=(1200,800),margin=5.0mm)
-    savefig(pt,"Output/Fig7/temp.png")
     return(nothing)
 end
 

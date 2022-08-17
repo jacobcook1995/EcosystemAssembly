be6d6d4bdeae220f1c58cf1593df2cde900a947c
Jacob Cook <jc2017@ic.ac.uk>
2022-08-17 15:16:00 +0100

    Added inset to figure 7

--- TradeOff/src/figures/Fig7.jl
+++ TradeOff/src/figures/Fig7.jl
@@ -51,16 +51,20 @@ function figure7(rps::Int64)
     # Load in averages
     mn_sbs = load(sfile,"mn_sbs")
     mn_via_R = load(sfile,"mn_via_R")
+    mn_KS_R = load(sfile,"mn_KS_R")
     # Load in standard deviations
     sd_sbs = load(sfile,"sd_sbs")
     sd_via_R = load(sfile,"sd_via_R")
+    sd_KS_R = load(sfile,"sd_KS_R")
     # Preallocate standard errors
     se_via_R = zeros(size(sd_via_R))
+    se_KS_R = zeros(size(sd_KS_R))
     # Calculate standard errors from this
     se_sbs = sd_sbs./sqrt.(no_sims)
     # Calculation (slightly) different in the viable case
     for i = 1:size(sd_via_R,1)
         se_via_R[i,:] = sd_via_R[i,:]./sqrt.(no_via)
+        se_KS_R[i,:] = sd_KS_R[i,:]./sqrt.(no_via)
     end
     # Preallocate probabilites
     mn_Ps = zeros(size(mn_via_R))
@@ -103,6 +107,17 @@ function figure7(rps::Int64)
     plot!(p2,times,1 .-mn_Ps[3,:],ribbon=(up_Ps[3,:],dw_Ps[3,:]),label="R=3",color=a[2])
     plot!(p2,times,1 .-mn_Ps[5,:],ribbon=(up_Ps[5,:],dw_Ps[5,:]),label="R=5",color=a[3])
     plot!(p2,times,1 .-mn_Ps[7,:],ribbon=(up_Ps[7,:],dw_Ps[7,:]),label="R=7",color=a[4])
+    # Define box for inset here
+    box = (1,bbox(0.5,0.5,0.45,0.3375,:bottom,:left))
+    Ks = L"K_S"
+    e7 = L"10^7"
+    em3 = L"10^{-3}"
+    plot!(p2,times/1e7,mn_KS_R[1,:]*1000.0,ribbon=se_KS_R[1,:]*1000.0,label="R=1",color=a[1],inset_subplots=box,subplot=2)
+    plot!(p2,times/1e7,mn_KS_R[3,:]*1000.0,ribbon=se_KS_R[3,:]*1000.0,label="R=3",color=a[2],subplot=2)
+    plot!(p2,times/1e7,mn_KS_R[5,:]*1000.0,ribbon=se_KS_R[5,:]*1000.0,label="R=5",color=a[3],subplot=2)
+    plot!(p2,times/1e7,mn_KS_R[7,:]*1000.0,ribbon=se_KS_R[7,:]*1000.0,label="R=7",color=a[4],subplot=2)
+    plot!(p2,xlim=(-Inf,0.25),guidefontsize=9,grid=false,legend=false,subplot=2)
+    plot!(p2,xlabel="Time ($(e7) s)",ylabel="$(Ks) ($em3)",subplot=2)
     # Add annotation
     px, py = annpos([0.0;2.5e6],[0.0;1.1],0.075,0.05)
     annotate!(p2,[px,py,text("B",17,:black)])

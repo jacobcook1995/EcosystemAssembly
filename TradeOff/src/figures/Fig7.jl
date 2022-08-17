60613d731e1e0c0add33707e4ec70e9a712ba325
Jacob Cook <jc2017@ic.ac.uk>
2022-08-17 14:09:52 +0100

    Removed surplus no imm plots

--- TradeOff/src/figures/Fig7.jl
+++ TradeOff/src/figures/Fig7.jl
@@ -51,22 +51,13 @@ function figure7(rps::Int64)
     # Load in averages
     mn_sbs = load(sfile,"mn_sbs")
     mn_via_R = load(sfile,"mn_via_R")
-    mn_via_ω = load(sfile,"mn_via_ω")
-    mn_via_ϕR = load(sfile,"mn_via_ϕR")
-    mn_via_η = load(sfile,"mn_via_η")
     # Load in standard deviations
     sd_sbs = load(sfile,"sd_sbs")
     sd_via_R = load(sfile,"sd_via_R")
-    sd_via_ω = load(sfile,"sd_via_ω")
-    sd_via_ϕR = load(sfile,"sd_via_ϕR")
-    sd_via_η = load(sfile,"sd_via_η")
     # Preallocate standard errors
     se_via_R = zeros(size(sd_via_R))
     # Calculate standard errors from this
     se_sbs = sd_sbs./sqrt.(no_sims)
-    se_via_ω = sd_via_ω./sqrt.(no_via)
-    se_via_ϕR = sd_via_ϕR./sqrt.(no_via)
-    se_via_η = sd_via_η./sqrt.(no_via)
     # Calculation (slightly) different in the viable case
     for i = 1:size(sd_via_R,1)
         se_via_R[i,:] = sd_via_R[i,:]./sqrt.(no_via)
@@ -96,13 +87,13 @@ function figure7(rps::Int64)
     a = ColorSchemes.sunset.colors
     # Plot basic trade-off first
     p1 = plot(xlabel="Time (s)",ylabel="Number of species",xlim=(-Inf,2.5e6))
-    plot!(p1,title="Number of reactions with time",legend=:bottomright)
+    plot!(p1,title="Number of reactions with time",legend=:bottomright,ylim=(0.0,7.5))
     plot!(p1,times,mn_via_R[1,:],ribbon=se_via_R[1,:],label="R=1",color=a[1])
     plot!(p1,times,mn_via_R[3,:],ribbon=se_via_R[3,:],label="R=3",color=a[2])
     plot!(p1,times,mn_via_R[5,:],ribbon=se_via_R[5,:],label="R=5",color=a[3])
     plot!(p1,times,mn_via_R[7,:],ribbon=se_via_R[7,:],label="R=7",color=a[4])
     # Add annotation
-    px, py = annpos([0.0;2.5e6],[0.0;10.0],0.05,0.0)
+    px, py = annpos([0.0;2.5e6],[0.0;7.5],0.075,0.05)
     annotate!(p1,[px,py,text("A",17,:black)])
     savefig(p1,"Output/Fig7/AvViaReacsTime.png")
     # Now do probability plot
@@ -113,39 +104,11 @@ function figure7(rps::Int64)
     plot!(p2,times,1 .-mn_Ps[5,:],ribbon=(up_Ps[5,:],dw_Ps[5,:]),label="R=5",color=a[3])
     plot!(p2,times,1 .-mn_Ps[7,:],ribbon=(up_Ps[7,:],dw_Ps[7,:]),label="R=7",color=a[4])
     # Add annotation
-    px, py = annpos([0.0;2.5e6],[0.0;1.1],0.05,0.0)
+    px, py = annpos([0.0;2.5e6],[0.0;1.1],0.075,0.05)
     annotate!(p2,[px,py,text("B",17,:black)])
     savefig(p2,"Output/Fig7/ProbSubTime.png")
-    # Load in color scheme
-    a = ColorSchemes.Dark2_4.colors
-    # Make box for the subplots
-    box = (1,bbox(0.5,0.65,0.4,0.3,:bottom,:left))
-    # Define reused latex strings
-    e6 = L"10^6"
-    # Make plot objects
-    p3 = plot(xlabel="Time (s)",xlim=(-Inf,2.5e6),ylim=(0.5,0.65),legend=:topleft)
-    plot!(p3,title="Ribosome dynamics",ylabel="Maximum ribsome fraction factor ($(L"\omega"))")
-    # Add inset box to plot other trade-off into
-    plot!(p3,xlabel="Time ($(e6) s)",ylabel=L"\phi_R",inset_subplots=box,subplot=2,grid=false,legend=false)
-    plot!(p3,subplot=2,xlim=(-Inf,2.5),ylim=(0.05,0.3),legend=false)
-    # Then actually plot the relevant data
-    plot!(p3,times,mn_via_ω,ribbon=se_via_ω,label="",color=a[1])
-    plot!(p3,times/1e6,mn_via_ϕR,ribbon=se_via_ϕR,color=a[1],subplot=2)
-    # Add annotation
-    px, py = annpos([0.0;2.5e6],[0.0;0.654],0.1,0.0)
-    annotate!(p3,[px,py,text("C",17,:black)])
-    savefig(p3,"Output/Fig7/omega_with_t.png")
-    # Make plot objects
-    p4 = plot(xlabel="Times (s)",xlim=(-Inf,2.5e6),legend=:right,ylabel=L"\eta",title="ATP yield with time")
-    # Plot the data to the relevant plot objects
-    plot!(p4,times,mn_via_η,ribbon=se_via_η,color=a[1],label="")
-    # Add annotation
-    px, py = annpos([0.0;2.5e6],[0.0;3.385],0.05,0.0)
-    annotate!(p4,[px,py,text("D",17,:black)])
-    # Save figures to this directory
-    savefig(p4,"Output/Fig7/Eta.png")
     # Plot all graphs as a single figure
-    pt = plot(p1,p3,p2,p4,layout=4,size=(1200,800),margin=5.0mm)
+    pt = plot(p1,p2,layout=(2,1),size=(600,800),margin=5.0mm)
     savefig(pt,"Output/Fig7/figure7.png")
     return(nothing)
 end

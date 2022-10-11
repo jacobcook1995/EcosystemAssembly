0364b8db6b7288b1eeed85ce859b233085d761c4
Jacob Cook <jc2017@ic.ac.uk>
2022-10-11 08:12:56 +0100

    Updated figure to use biomass weighting

--- TradeOff/src/figures/Fig5.jl
+++ TradeOff/src/figures/Fig5.jl
@@ -50,19 +50,19 @@ function figure5(rps::Int64,ims::Int64)
     # Define reused latex strings
     e6 = L"10^6"
     # Make plot objects
-    p1 = plot(xlabel="Time (s)",xlim=(-Inf,5e7),ylim=(0.5,0.65),legend=:topleft)
+    p1 = plot(xlabel="Time (s)",xlim=(-Inf,5e7),ylim=(0.35,0.55),legend=:topleft)
     plot!(p1,title="Variation with free energy",ylabel="Maximum ribsome fraction factor ($(L"\omega"))")
     # Add inset box to plot other trade-off into
     plot!(p1,xlabel="Time ($(e6) s)",ylabel=L"\phi_R",inset_subplots=box,subplot=2,grid=false,legend=false)
     plot!(p1,subplot=2,xlim=(-Inf,10.0),ylim=(0.05,0.3),legend=false)
     # Now make second plot
-    p2 = plot(xlabel="Time (s)",xlim=(-Inf,5e7),ylim=(0.5,0.65),legend=:topleft)
+    p2 = plot(xlabel="Time (s)",xlim=(-Inf,5e7),ylim=(0.4,0.55),legend=:topleft)
     plot!(p2,title="Variation with maintenance cost",ylabel="Maximum ribsome fraction factor ($(L"\omega"))")
     # Add the same inset box, to plot the other trade-off into
     plot!(p2,xlabel="Time ($(e6) s)",ylabel=L"\phi_R",inset_subplots=box,subplot=2,grid=false)
     plot!(p2,subplot=2,xlim=(-Inf,10.0),ylim=(0.05,0.3),legend=false)
     # Loop over the 3 conditions
-    for i = 1:3
+    for i = 1:4
         # Extract other simulation parameters from the function
         Np, Nt, M, d, μrange = sim_paras(i)
         # Read in appropriate files
@@ -82,25 +82,25 @@ function figure5(rps::Int64,ims::Int64)
         times = load(tfile,"times")
         no_via = load(tfile,"no_via")
         # Load in averages
-        mn_via_ω = load(tfile,"mn_via_ω")
+        mn_via_ω_bw = load(tfile,"mn_via_ω_bw")
         mn_via_ϕR = load(tfile,"mn_via_ϕR")
         # Load in standard deviations
-        sd_via_ω = load(tfile,"sd_via_ω")
+        sd_via_ω_bw = load(tfile,"sd_via_ω_bw")
         sd_via_ϕR = load(tfile,"sd_via_ϕR")
         # Calculate relevant standard errors
-        se_via_ω = sd_via_ω./sqrt.(no_via)
+        se_via_ω_bw = sd_via_ω_bw./sqrt.(no_via)
         se_via_ϕR = sd_via_ϕR./sqrt.(no_via)
         # Plot the data to the relevant plot objects
         if i == 1 || i == 2
             # Find appropriate label
             lb = find_label(i,1)
-            plot!(p1,times,mn_via_ω,ribbon=se_via_ω,label=lb,color=a[i])
+            plot!(p1,times,mn_via_ω_bw,ribbon=se_via_ω_bw,label=lb,color=a[i])
             plot!(p1,times/1e6,mn_via_ϕR,ribbon=se_via_ϕR,color=a[i],subplot=2)
         end
         if i == 1 || i == 3
             # Find appropriate label
             lb = find_label(i,2)
-            plot!(p2,times,mn_via_ω,ribbon=se_via_ω,label=lb,color=a[i])
+            plot!(p2,times,mn_via_ω_bw,ribbon=se_via_ω_bw,label=lb,color=a[i])
             plot!(p2,times/1e6,mn_via_ϕR,ribbon=se_via_ϕR,color=a[i],subplot=2)
         end
     end
@@ -112,10 +112,10 @@ function figure5(rps::Int64,ims::Int64)
     savefig(p1,"Output/Fig5/omega_with_DG.png")
     savefig(p2,"Output/Fig5/omega_with_d.png")
     # Add annotations
-    px, py = annpos([0.0;5e7],[0.5;0.66],0.075,0.0)
-    annotate!(p1,[px,py,text("A",17,:black)])
-    px, py = annpos([0.0;5e7],[0.5;0.66],0.075,0.0)
-    annotate!(p2,[px,py,text("B",17,:black)])
+    px, py = annpos([0.0;5e7],[0.35;0.55],0.075,0.0)
+    annotate!(p1,px,py,text("A",17,:black))
+    px, py = annpos([0.0;5e7],[0.4;0.55],0.075,0.0)
+    annotate!(p2,px,py,text("B",17,:black))
     # Plot all graphs as a single figure
     pt = plot(p1,p2,layout=(2,1),size=(600,800),margin=5.0mm)
     savefig(pt,"Output/Fig5/figure5.png")

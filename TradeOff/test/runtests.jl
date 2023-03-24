aceb17f9079e9548420f66694ad45ca246188a85
Jacob Cook <jc2017@ic.ac.uk>
2023-03-24 11:46:19 +0000

    Added two very simple tests

--- TradeOff/test/runtests.jl
+++ TradeOff/test/runtests.jl
@@ -1,4 +1,8 @@
 using Test
+using TradeOff
 
-# An example test
-@test 1 == 1
+# WHEN I ADD NEW FUNCTIONS ADD TESTS FOR THEM IN HERE
+
+# Test functions defined in TradeOff
+@test Q(10.0, 2.0) == 1.0 / 5.0
+@test Keq(293.0, 1.0, -1e5) == 28628.16592393438

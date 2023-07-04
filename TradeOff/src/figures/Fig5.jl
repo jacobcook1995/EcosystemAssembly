742c10860beaa399b432745f44505ff6c1f50859
Jacob Cook <jc2017@ic.ac.uk>
2023-07-04 14:53:22 +0100

    Removed subplots from figure 5

--- TradeOff/src/figures/Fig5.jl
+++ TradeOff/src/figures/Fig5.jl
@@ -44,43 +44,20 @@ function figure5(ims::Int64)
     default(dpi = 200)
     # Load in colour scheme
     a = ColorSchemes.Dark2_4.colors
-    # Make box for the subplots
-    box = (1, bbox(0.5, 0.65, 0.4, 0.3, :bottom, :left))
-    # Define reused latex strings
-    e6 = L"10^6"
     # Make plot objects
     p1 = plot(xlabel = "Time (s)",
               xlim = (-Inf, 5e7),
-              ylim = (0.35, 0.55),
+              ylim = (0.5, 0.65),
               legend = :topleft,
               title = "Variation with free energy",
               ylabel = "Maximum ribosome fraction factor ($(L"\omega"))")
-    # Add inset box to plot other trade-off into
-    plot!(p1,
-          xlabel = "Time ($(e6) s)",
-          ylabel = L"\phi_R",
-          inset_subplots = box,
-          subplot = 2,
-          grid = false,
-          legend = false)
-    plot!(p1, subplot = 2, xlim = (-Inf, 10.0), ylim = (0.05, 0.3), legend = false)
     # Now make second plot
     p2 = plot(xlabel = "Time (s)",
               xlim = (-Inf, 5e7),
-              ylim = (0.4, 0.55),
+              ylim = (0.5, 0.65),
               legend = :topleft,
               title = "Variation with maintenance cost",
               ylabel = "Maximum ribosome fraction factor ($(L"\omega"))")
-    # Add the same inset box, to plot the other trade-off into
-    plot!(p2,
-          xlabel = "Time ($(e6) s)",
-          ylabel = L"\phi_R",
-          inset_subplots = box,
-          subplot = 2,
-          grid = false)
-    plot!(p2, subplot = 2, xlim = (-Inf, 10.0), ylim = (0.05, 0.3), legend = false)
-    p3 = plot(xlabel = "Time (s)", xlim = (-Inf, 5e7), legend = :topleft)
-    p4 = plot(xlabel = "Time (s)", xlim = (-Inf, 5e7), legend = :topleft)
     # Loop over the 3 conditions
     for i in 1:3
         # Extract other simulation parameters from the function
@@ -93,33 +70,23 @@ function figure5(ims::Int64)
         end
         # Now load out the times, and number of trajectories
         times = load(tfile, "times")
-        no_via = load(tfile, "no_via")
+        no_via = load(tfile, "no_viable_simulations")
         # Load in averages
-        mn_via_ω_bw = load(tfile, "mn_via_ω_bw")
-        mn_via_ϕR = load(tfile, "mn_via_ϕR")
-        mn_via_a = load(tfile, "mn_via_a")
+        mn_via_ω_bw = load(tfile, "mean_average_ω")
         # Load in standard deviations
-        sd_via_ω_bw = load(tfile, "sd_via_ω_bw")
-        sd_via_ϕR = load(tfile, "sd_via_ϕR")
-        sd_via_a = load(tfile, "sd_via_a")
+        sd_via_ω_bw = load(tfile, "sd_average_ω")
         # Calculate relevant standard errors
         se_via_ω_bw = sd_via_ω_bw ./ sqrt.(no_via)
-        se_via_ϕR = sd_via_ϕR ./ sqrt.(no_via)
-        se_via_a = sd_via_a ./ sqrt.(no_via)
         # Plot the data to the relevant plot objects
         if i == 1 || i == 2
             # Find appropriate label
             lb = find_label(i, 1)
             plot!(p1, times, mn_via_ω_bw, ribbon = se_via_ω_bw, label = lb, color = a[i])
-            plot!(p1, times / 1e6, mn_via_ϕR, ribbon = se_via_ϕR, color = a[i], subplot = 2)
-            plot!(p3, times, mn_via_a, ribbon = se_via_a, label = lb, color = a[i])
         end
         if i == 1 || i == 3
             # Find appropriate label
             lb = find_label(i, 2)
             plot!(p2, times, mn_via_ω_bw, ribbon = se_via_ω_bw, label = lb, color = a[i])
-            plot!(p2, times / 1e6, mn_via_ϕR, ribbon = se_via_ϕR, color = a[i], subplot = 2)
-            plot!(p4, times, mn_via_a, ribbon = se_via_a, label = lb, color = a[i])
         end
     end
     # Check if directory exists and if not make it
@@ -129,8 +96,6 @@ function figure5(ims::Int64)
     # Save figures to this directory
     savefig(p1, "Output/Fig5/omega_with_DG.png")
     savefig(p2, "Output/Fig5/omega_with_d.png")
-    savefig(p3, "Output/Fig5/a_with_DG.png")
-    savefig(p4, "Output/Fig5/a_with_d.png")
     # Add annotations
     px, py = annpos([0.0; 5e7], [0.35; 0.55], 0.075, 0.0)
     annotate!(p1, px, py, text("A", 17, :black))
